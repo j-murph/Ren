bda13351ab1b301d696a0b71dba929e4933b20f8
j-murph <lone_geek@live.com>
2023-03-19 22:31:08 -0700

    no message

--- SoftwareRen/Math.h
+++ SoftwareRen/Math.h
@@ -215,7 +215,7 @@ struct Vector2d
 		T length = Length();
 		if (length != static_cast<T>(0))
 		{
-			T inverseLength = 1 / inverseLength;
+			T inverseLength = 1 / length;
 			x *= inverseLength;
 			y *= inverseLength;
 		}
